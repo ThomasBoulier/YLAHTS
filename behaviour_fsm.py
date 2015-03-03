755e6201928241b9c79da37a4c14d6b3b8d55686
Thomas Boulier <ThomasBoulier@users.noreply.github.com>
2015-03-03 10:52:17 +0100

    Update behaviour_fsm.py
    upadate en cours, mise en place des etats et transitions

--- behaviour_fsm.py
+++ behaviour_fsm.py
@@ -1,5 +1,9 @@
 import fsm
-# Manque des imports
+import sys
+sys.path.append(r"c:\Python34\Lib\site-packages")
+sys.path.append(r"c:\Python34\Lib")
+import serial, os, threading
+# Manque des imports ?
 
 # global variables
 f = fsm.fsm()   # defines finite state machine
@@ -10,16 +14,28 @@ def myFunct():
     print "do something ..."
     nextEvent = "???"
     return nextEvent
+    
+def Init():
+    print "init"
+    nextEvent = "init_done"
+    return nextEvent
+    
+def decollage():
+    print "decollage"
+    #corps de la fonction
+    nextEvent = "decollage_done"
+    return nextEvent 
 
 # Main 
 if __name__== "__main__":
 
     # init FSM
-    f.add_state ("???")
+    f.add_state ("init")    
+    f.add_state ("decollage")
     f.add_state ("???")
 
-    f.add_event ("??")
-    f.add_event ("??")
+    f.add_event ("init_done")
+    f.add_event ("decollage_done")
    
     f.add_transition ("??","??","??",myFunct);
     
