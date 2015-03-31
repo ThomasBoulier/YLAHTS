70769fb3ea081d33c813b193d4b3a93ce78c6c1d
Thomas Boulier <ThomasBoulier@users.noreply.github.com>
2015-03-31 15:17:57 +0200

    Update side.py

--- side.py
+++ side.py
@@ -2,4 +2,11 @@ import sys
 sys.path.append(r"c:\Python34\Lib\site-packages")
 sys.path.append(r"c:\Python34\Lib")
 import serial, os, threading
+import numpy as np
 
+def isThereAnObject:
+  liste = np.zeros(24)
+  b = objectInCam()
+  liste.append(b)
+  rate = liste.count(True)/24
+  return (rate > 0.8)
